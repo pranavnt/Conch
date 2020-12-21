487189c1e7c352e875b0b01b19577612fb210704
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-20 21:46:30 -0800

    testing Script struct works

--- api/api.go
+++ api/api.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("csh api")
+	entry := Script{
+		Name:      "pranav",
+		Script:    "echo hello world!",
+		UserName:  "pranavnt",
+		UserEmail: "pranavnt@outlook.com",
+	}
+
+	addScript(entry)
+}
+
+func addScript(script Script) {
+	scriptJSON, _ := json.Marshal(script)
+
+	fmt.Println(string(scriptJSON))
 }
 
 type Script struct {
-	name      string
-	script    string
-	userName  string
-	userEmail string
+	Name      string
+	Script    string
+	UserName  string
+	UserEmail string
 }
