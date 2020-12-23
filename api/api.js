698570592a4352df08b09c0c6623a1fb92511939
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-23 02:11:49 -0800

    finished /scripts route

--- api/api.js
+++ api/api.js
@@ -6,11 +6,19 @@ app.get("/scripts/:name", function (req, res) {
   let dataArr = JSON.parse(fs.readFileSync(path.resolve(__dirname, "db.json")))[
     "scripts"
   ];
-  res.json(dataArr);
+
+  for (var i = 0; i < dataArr.length; i++) {
+    console.log(dataArr[i]["name"]);
+    if (dataArr[i]["name"] == req.params.name) {
+      res.json({ script: dataArr[i]["script"] });
+      return;
+    }
+  }
+  res.json({ script: "echo Script not found" });
 });
 
 app.post("/upload", function (req, res) {});
 
 app.listen(3000, () => {
-  console.log("hi");
+  console.log("");
 });
