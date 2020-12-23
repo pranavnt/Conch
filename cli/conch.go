213a979dc1c0c4483b80037d0cbba28dcfe15736
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-23 00:55:05 -0800

    basic instructuions in runScript

--- cli/conch.go
+++ cli/conch.go
@@ -8,14 +8,28 @@ import (
 )
 
 func main() {
-	fmt.Println("csh cli")
-
 	runCmd("echo hi && echo bye")
 }
 
+func uploadScript(fileName string, name string) {
+
+}
+
+func runScript(name string) {
+	// call API to get script
+
+	// send GET request to server -- serverurl/scripts/name
+	url := "serverURL.com/scripts/" + name
+	fmt.Println(url)
+
+	// once you have the script, do runCmd to run the script
+}
+
 func runCmd(cmd string) {
+	// splits string of script into lines
 	lines := strings.Split(cmd, " && ")
 
+	// iterates through lines
 	for i := 0; i < len(lines); i++ {
 		line := strings.Split(lines[i], " ")
 
