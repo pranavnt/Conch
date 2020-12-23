9a4826dc5874377e16c3822082005e5586fae304
Pranav Teegavarapu <pranavnt@outlook.com>
2020-12-23 01:53:20 -0800

    reading db works

--- api/api.js
+++ api/api.js
@@ -1,1 +1,16 @@
 const app = require("express")();
+const fs = require("fs");
+const path = require("path");
+
+app.get("/scripts/:name", function (req, res) {
+  let dataArr = JSON.parse(fs.readFileSync(path.resolve(__dirname, "db.json")))[
+    "scripts"
+  ];
+  res.json(dataArr);
+});
+
+app.post("/upload", function (req, res) {});
+
+app.listen(3000, () => {
+  console.log("hi");
+});
